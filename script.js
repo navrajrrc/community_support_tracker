f5c1a9fbc73da13e12348241772a9e4a657e6bb5
Rajandeep1 <rajandeepkaur2@rrc.ca>
2025-04-18 15:06:35 -0500

    error message shown in the form

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const charityname = document.getElementById('charityname');
+const donationamount = document.getElementById('number');
+const donationdate = document.getElementById('donationdate');
+const donorcomment = document.getElementById('donorcomment');
+const donationform = document.getElementById('donation-form');
+ 
+const charitynameerror = document.getElementById('charityname-error');
+const donationamounterror = document.getElementById('donationamount-error');
+const donationdateerror = document.getElementById('donationdate-error');
+const donorcommenterror = document.getElementById('donorcomment-error');
+ 
+ 
+donationform.addEventListener('submit', (e) =>
+{
+    e.preventDefault();
+ 
+    charitynameerror.innerHTML = "";
+ 
+    if(charityname.value === "" || charityname.value === null){
+       
+       charitynameerror.innerHTML = "please enter a charity name";
+    }
+ 
+    donationamounterror.innerHTML = "";
+    if (donationamount.value === "" || donationamount.value <= 0){
+        donationamounterror.innerHTML = "please enter a valid donation amount";
+    }
+ 
+    donationdateerror.innerHTML = "";
+    if (donationdate.value === "" ){
+        donationdateerror.innerHTML = "please enter a donation date";
+ 
+    }
+ 
+    donorcommenterror.innerHTML = "";
+    if(donorcomment.value === "" || donorcomment.value === null){
+       
+        donorcommenterror.innerHTML = "please enter a comment";
+    }
+ 
+});
+
 const hamburger = document.querySelector('.hamburger');
 const topnav = document.querySelector('.topnav');
  
